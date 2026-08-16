81277a792e324750ce5c645bf664914c936ac62a
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-08-16 12:29:35 +0100

    migrate from mdzjs to @zikojs/mdx

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 // import { MDZ } from "../src/vite/index.js";
 // import ViteMDZ  from "../src/bundlers/vite.js"
-import ViteMDZ from "mdzjs/vite"
+import ViteMDZ from "@zikojs/mdx/vite"
 
 export default defineConfig({
     plugins : [ViteMDZ()]
